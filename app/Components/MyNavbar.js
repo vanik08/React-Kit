76409eed4386b26bc5e6240748c38b0e608eb49d
vanik08 <vanik08@gmail.com>
2015-09-07 12:39:38 -0700

    Fixed issue where navbar with stay open on mobile devices when link changed

--- app/Components/MyNavbar.js
+++ app/Components/MyNavbar.js
@@ -4,16 +4,20 @@ import {Nav, Navbar, NavItem} from 'react-bootstrap';
 export default class MyNavbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.handleExpand = this.handleExpand.bind(this);
+    this.state = {expand: false}
+  }
+  handleExpand(e) {
+    this.setState({expand: !this.state.expand});
   }
   render() {
     return (
       <div>
-        <Navbar brand='React' fluid toggleNavKey={0}>
+        <Navbar brand='React' fluid toggleNavKey={0} navExpanded={this.state.expand} onToggle={this.handleExpand}>
           <Nav eventKey={0}>
-            <NavItem eventKey={1} href='#/'>Home</NavItem>
-            <NavItem eventKey={2} href='#/about'>About</NavItem>
-            <NavItem eventKey={3} href='#/contact'>Contact</NavItem>
+            <NavItem onClick={this.handleExpand} eventKey={1} href='#/'>Home</NavItem>
+            <NavItem onClick={this.handleExpand} eventKey={2} href='#/about'>About</NavItem>
+            <NavItem onClick={this.handleExpand} eventKey={3} href='#/contact'>Contact</NavItem>
           </Nav>
         </Navbar>
       </div>
