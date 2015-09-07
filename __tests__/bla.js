67406bd45907f885de09db67a6501d34604752a5
vanik08 <vanik08@gmail.com>
2015-09-07 16:54:19 -0700

    Implemented Jest for unit-testing the app

---
+++ __tests__/bla.js
@@ -0,0 +1,10 @@
+describe('sum', function() {
+ it('adds 1 + 2 to equal 3', function() {
+   expect(3).toBe(3);
+ });
+});
+describe('sums', function() {
+ it('adds 1 + 2 to equal 3', function() {
+   expect(6).toBe(3);
+ });
+});
