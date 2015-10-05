17bd1c68c8cb72a54daafd2d1e8a0974e1372b27
vanik08 <vanik08@gmail.com>
2015-10-05 11:20:18 -0700

    Cleanup to follow ESLint

--- app/app.js
+++ app/app.js
@@ -13,11 +13,6 @@ import 'styles/app';
 
 import {sideNav} from 'styles/styles';
 
-//test
-import Datepicker from 'components/Datepicker';
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -59,7 +54,6 @@ class App extends React.Component {
           <Col md={10}>
             <h5>{this.state.text}</h5>
             <RouteHandler />
-            <Datepicker />
           </Col>
         </Row>
       </div>
